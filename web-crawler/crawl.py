245f4203001e9af4e14da7e9c579862dec11adb8
Raquel Calderon <calderon.rocky@gmail.com>
2014-06-30 12:55:37 -0700

    Deleted food and pet

--- web-crawler/crawl.py
+++ web-crawler/crawl.py
@@ -4,15 +4,17 @@ from bs4 import BeautifulSoup
 import openpyxl
 from openpyxl import Workbook, cell
 
+
 def get_start_url(listing):
     if listing.endswith('/'):
         if listing.startswith('w'):
-            listings="http://"+listing+where.capitalize()+'/'+what.capitalize()
+            listings = "http://" + listing + where.capitalize() + '/' + what.capitalize()
         else:
-            listings=listing+where.capitalize()+'/'+what.capitalize()
+            listings = listing + where.capitalize() + '/' + what.capitalize()
     else:
-        listings="http://"+listing+'/'+where.capitalize()+'/'+what.capitalize()
+        listings = "http://" + listing + '/' + where.capitalize() + '/' + what.capitalize()
     return listings
+
 
 def get_html(url):
     """
@@ -23,10 +25,11 @@ def get_html(url):
     message = requests.get(url)
     return message
 
+
 def get_next_link(msg):
     class_regex = re.compile('<div\sclass=[\'"]pagination[\'"]>(.*?)</div>')
     a_list = class_regex.findall(msg)
-    if len(a_list)>0:
+    if len(a_list) > 0:
         a = a_list[0].split('</a>')
         a = [i for i in a if 'next' in i]
         link_regex = re.compile('<a\shref=[\'"](.*?)[\'"]>next\s')
@@ -38,29 +41,35 @@ def get_next_link(msg):
     else:
         return ''
 
+
 def get_company_name(soup):
-    c_name=soup.findAll(attrs={"class":"jrat"})
-    cname=[]
+    print('in get company name')
+    c_name = soup.findAll(attrs={"class": "jrat"})
+    cname = []
     for i in c_name:
         cname.append(i.text)
     return cname
 
+
 def get_phone(soup):
-    divtag=soup.findAll(attrs={"class":"ctc f13"})
-    phone=[i.p.text.replace('Call:','') for i in divtag]
+    divtag = soup.findAll(attrs={"class": "ctc f13"})
+    phone = [i.p.text.replace('Call:', '') for i in divtag]
     return phone
 
+
 def get_add(soup):
-    add_list=soup.findAll(attrs={"class":"jrc13"})
-    add=[i.text.split('|')[0].replace('\t','') for i in add_list]
+    add_list = soup.findAll(attrs={"class": "jrc13"})
+    add = [i.text.split('|')[0].replace('\t', '') for i in add_list]
     return add
+
 
 if __name__ == '__main__':
     listing = "www.justdial.com"
     where = input("City: ")
     what = input("Category: ")
     url = get_start_url(listing)
-   
+    print(len(url))
+
     name = []
     person = []
     phone = []
@@ -68,34 +77,34 @@ if __name__ == '__main__':
 
     while url:
         if ' ' in url:
-            url = url.replace(' ','%20')
+            url = url.replace(' ', '%20')
             print(url)
         msg = requests.get(url).text
         print(url)
         soup = BeautifulSoup(msg)
         for i in get_company_name(soup):
             name.append(i)
-        for i in get_phone(soup):   
+        for i in get_phone(soup):
             phone.append(i)
         for i in get_add(soup):
             add.append(i)
-        url=get_next_link(msg)
+        url = get_next_link(msg)
     book = openpyxl.Workbook()
     sheet1 = book.active
-    index = ['NAME','PHONE','ADDRESS']
-    #style = xlwt.XFStyle()
+    index = ['NAME', 'PHONE', 'ADDRESS']
+    # style = xlwt.XFStyle()
     #font = xlwt.Font()
     #font.name = 'Times New Roman'
     #font.bold = True
     #style.font = font
-    
-    # for n in range(0,3):
-        # sheet1.write(0,n,index[n].upper(),style)
-        
-    for i in range(0,len(name)):
-        sheet1.cell('%s%s'%(i+1,0)).value = name[i]
-        sheet1.cell('%s%s'%(i+1,1)).value = phone[i]
-        sheet1.cell('%s%s'%(i+1,2)).value = add[i]
 
-    book.save(what+'.xlsx')
+    # for n in range(0,3):
+    # sheet1.write(0,n,index[n].upper(),style)
+
+    for i in range(0, len(name)):
+        sheet1.cell('%s%s' % (i + 1, 0)).value = name[i]
+        sheet1.cell('%s%s' % (i + 1, 1)).value = phone[i]
+        sheet1.cell('%s%s' % (i + 1, 2)).value = add[i]
+
+    book.save(what + '.xlsx')
         
